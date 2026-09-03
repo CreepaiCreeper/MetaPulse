69e1a25e28d2cb4b9a4cba6de76eee5530c10cc9
CreepaiCreeper <creeperxd8758@gmail.com>
2026-09-03 17:01:32 +0300

    ggg

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -101,7 +101,7 @@ const Pricing = () => {
         </div>
 
         {/* pricing cards */}
-        <div className="w-full max-w-3xl mx-auto z-20 mt-10 md:mt-20">
+        <div className="w-full max-w-3xl mx-auto z-20 md:mt-20">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch">
             {plans.map((plan) => (
               <div
